fa49b5009b0440d4f2ca9cd6e13166fb924c78b7
IGroll <131527984+IGroll@users.noreply.github.com>
2023-05-20 14:29:36 +0500

    Update ping-pong.py
    ping-pong v???

--- ping-pong.py
+++ ping-pong.py
@@ -9,6 +9,7 @@ end = True
 wait = False
 blue_goals = 0
 red_goals = 0
+randomised_num = 0
 font = font.SysFont('Arial',70)
 blue = font.render(str(blue_goals),True,(0,50,255))
 red = font.render(str(red_goals),True,(255,50,50))
@@ -17,6 +18,9 @@ wait_schetchik = 3
 aaa = 0
 rounds = 1
 chanse = 100
+timer_event = False
+randomised_num = 1
+timer_event_i = 0
 round = font.render('Раунд:'+str(rounds),True,(0,0,0))
 ten_time = 0
 one_time = 0
@@ -73,26 +77,42 @@ def calibrate_timer_and_events():
     global second_timer
     global minut_timer
     global chanse
+    global timer_event
+    global timer_event_i
+    global end
     randomised_num = randint(1,chanse)
     if second_timer > 9:
         tsecond_timer += 1
         second_timer = 0
-        chanse = chanse-chanse//33
-        print(chanse)
+        chanse = chanse-chanse//20
     if tsecond_timer == 6:
         minut_timer += 1
         tsecond_timer = 0 
         ball.xspeed += 3
         ball.yspeed += 3
+        ball2.xspeed += 3
+        ball2.yspeed += 3
+    if minut_timer >= 3 and end:
+        ball2.update()
     if minut_timer > 9:
         tminut_timer += 1
         minut_timer = 0
     if randomised_num == 1:
         player1.speed *= -1
         player2.speed *= -1
-player1 = Player(5,'player1.png',650,220)
-player2 = Player(5,'player2.png',100,220)
+        timer_event_i = 0
+        timer_event = True
+    if timer_event:
+        timer_event_i += 1
+    if timer_event_i > 9:
+        player1.speed = 5
+        player2.speed = 5
+        timer_event_i = 0
+        timer_event = False
+player1 = Player(10,'player1.png',650,220)
+player2 = Player(10,'player2.png',100,220)
 ball = Ball(3,'ball.png',400,100)
+ball2 = Ball(3,'ball.png',400,100)
 second_timer = 0
 minut_timer = 0
 tsecond_timer = 0
@@ -113,11 +133,11 @@ while game:
         win = font.render('win blue',True,(0,0,255))
         window.blit(win,(290,250)) 
         wait = False
-    calibrate_timer_and_events()
     timer = font.render(str(tminut_timer)+str(minut_timer)+':'+str(tsecond_timer)+str(second_timer),True,(0,0,0))
     timer_for_timer += 1
     if timer_for_timer >= fps:
         second_timer += 1
+        calibrate_timer_and_events()
         timer_for_timer = 0
     window.blit(timer,(330,30))
     if wait:
@@ -128,6 +148,8 @@ while game:
             wait_schetchik = 3
             ball.xspeed = 3
             ball.yspeed = 3
+            ball2.xspeed = 3
+            ball2.yspeed = 3
             wait = False
             end = True
         window.blit(round,(290,250))   
@@ -139,23 +161,27 @@ while game:
         player2.update()
         player2.move2()
         ball.update()
-        if ball.rect.x <= -110:
+        if ball.rect.x <= -110 or ball2.rect.x <= -110:
             rounds += 1
             blue_goals += 1
             blue = font.render(str(blue_goals),True,(0,50,255))
             ball.rect.x = 400
             ball.rect.y = 300
+            ball2.rect.x = 400
+            ball2.rect.y = 300
             end = False
             wait = True
             player1.rect.y = 220
             player2.rect.y = 220
         window.blit(blue,(650,50))    
-        if ball.rect.x >= 910:
+        if ball.rect.x >= 910 or ball2.rect.x >= 910:
             rounds += 1
             red_goals += 1
             red = font.render(str(red_goals),True,(255,50,50))
             ball.rect.x = 400
             ball.rect.y = 300
+            ball2.rect.x = 400
+            ball2.rect.y = 300
             end = False
             wait = True
             player1.rect.y = 220
